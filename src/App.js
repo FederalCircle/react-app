004fb4335b82c2fc56adab50cc8b152fef73a519
Alyson Maia <alyson.maia@gmail.com>
2018-04-12 22:50:02 -0300

    Clean post module 3 task

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
-
 class App extends Component {
-  state = {
-    userName: 'Lorem Ipsum'
-  }
-
-  onChangeHandler = (event) => {
-    this.setState({
-      userName: event.target.value
-    });
-  }
-
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
-        <UserInput
-          changeRef={this.onChangeHandler}
-          userName={this.state.userName} />
-        <UserOutput
-          userName={this.state.userName} />
       </div>
     );
   }
